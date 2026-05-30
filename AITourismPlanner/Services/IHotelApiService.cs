f42d02772a7e0be53b60325176f220e737d1ecda
Shanza Naveed <shanzanaveed005@gmail.com>
2026-05-31 01:16:33 +0500

    added transport functionality

--- AITourismPlanner/Services/IHotelApiService.cs
+++ AITourismPlanner/Services/IHotelApiService.cs
@@ -1,4 +1,8 @@
-﻿namespace AITourismPlanner.Services
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AITourismPlanner.Services
 {
     public interface IHotelApiService
     {
---
+++ AITourismPlanner/Services/ITransportService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AITourismPlanner.Services
+{
+    public interface ITransportService
+    {
+        Task<List<TransportOption>> GetTransportOptionsAsync(string from, string to, DateTime date);
+    }
+
+    public class TransportOption
+    {
+        public string Type { get; set; } = string.Empty;
+        public string Company { get; set; } = string.Empty;
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public string Duration { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int AvailableSeats { get; set; }
+    }
+}
